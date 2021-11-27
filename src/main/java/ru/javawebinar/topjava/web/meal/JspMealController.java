353463bccf1b9d34805008a62751a6dd0c0db185
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2021-11-27 12:16:59 +0200

    HW6

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -0,0 +1,96 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+@Controller
+@RequestMapping(value = "/meals")
+public class JspMealController {
+
+    @Autowired
+    private MealService mealService;
+
+    @GetMapping
+    public String getMeals(HttpServletRequest request, Model model){
+        LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+        LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+        LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+        LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+        int userId = SecurityUtil.authUserId();
+        model.addAttribute("meals",
+                MealsUtil.getFilteredTos(mealService.getBetweenInclusive(
+                        startDate, endDate, userId), SecurityUtil.authUserCaloriesPerDay(), startTime, endTime));
+        return "meals";
+    }
+
+    @GetMapping("/delete")
+    public String delete(HttpServletRequest request) {
+        mealService.delete(getId(request), SecurityUtil.authUserId());
+        return "redirect:/meals";
+    }
+
+    @GetMapping("/update")
+    public String getUpdateForm(HttpServletRequest request, Model model) {
+        Meal meal = mealService.get(getId(request), SecurityUtil.authUserId());
+        model.addAttribute("meal", meal);
+        return "editMealForm";
+    }
+
+    @GetMapping("/create")
+    public String getCreateForm(HttpServletRequest request, Model model) {
+        Meal meal = new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000);
+        model.addAttribute("meal", meal);
+        return "mealForm";
+    }
+
+    @PostMapping("/update")
+    public String update(HttpServletRequest request) {
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+        assureIdConsistent(meal, getId(request));
+       mealService.update(meal,SecurityUtil.authUserId());
+        return "redirect:/meals";
+    }
+
+    @PostMapping("/create")
+    public String create(HttpServletRequest request) {
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+        checkNew(meal);
+        mealService.create(meal, SecurityUtil.authUserId());
+        return "redirect:/meals";
+    }
+
+
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.parseInt(paramId);
+    }
+
+
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/editMealForm.jsp
@@ -0,0 +1,33 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<html>
+<jsp:include page="fragments/headTag.jsp"/>
+<body>
+<jsp:include page="fragments/bodyHeader.jsp"/>
+<section>
+  <h3><a href="${pageContext.request.contextPath}">Home</a></h3>
+  <hr>
+  <h2>Edit meal</h2>
+  <jsp:useBean id="meal" type="ru.javawebinar.topjava.model.Meal" scope="request"/>
+  <form method="post" action="${pageContext.request.contextPath}/meals/update">
+    <input type="hidden" name="id" value="${meal.id}">
+    <dl>
+      <dt>DateTime:</dt>
+      <dd><input type="datetime-local" value="${meal.dateTime}" name="dateTime" required></dd>
+    </dl>
+    <dl>
+      <dt>Description:</dt>
+      <dd><input type="text" value="${meal.description}" size=40 name="description" required></dd>
+    </dl>
+    <dl>
+      <dt>Calories:</dt>
+      <dd><input type="number" value="${meal.calories}" name="calories" required></dd>
+    </dl>
+    <button type="submit">Save</button>
+    <button onclick="window.history.back()" type="button">Cancel</button>
+  </form>
+</section>
+<jsp:include page="fragments/footer.jsp"/>
+</body>
+</html>
