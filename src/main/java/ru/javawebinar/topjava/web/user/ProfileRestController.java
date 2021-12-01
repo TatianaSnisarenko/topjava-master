f78df0709f9c36295a5545b0cbf6132868085971
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2021-12-01 16:56:34 +0200

    create AdminRestController and ProfileRestController

--- src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
@@ -1,22 +1,37 @@
 package ru.javawebinar.topjava.web.user;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import ru.javawebinar.topjava.model.User;
 
 import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
 
-@Controller
+@RestController
+@RequestMapping(value = ProfileRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProfileRestController extends AbstractUserController {
+    static final String REST_URL = "/rest/profile";
 
+    @GetMapping
     public User get() {
         return super.get(authUserId());
     }
 
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete() {
         super.delete(authUserId());
     }
 
-    public void update(User user) {
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody User user) {
         super.update(user, authUserId());
     }
 }
