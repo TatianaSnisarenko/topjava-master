231349ecf8c345a058dd11ba15b43e8d4b56bd82
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2021-12-01 16:55:43 +0200

    migrate to junit 5

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -1,7 +1,7 @@
 package ru.javawebinar.topjava.service.datajpa;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
@@ -16,17 +16,17 @@ import static ru.javawebinar.topjava.UserTestData.USER_MATCHER;
 import static ru.javawebinar.topjava.UserTestData.admin;
 
 @ActiveProfiles(DATAJPA)
-public class DataJpaMealServiceTest extends AbstractMealServiceTest {
+class DataJpaMealServiceTest extends AbstractMealServiceTest {
     @Test
-    public void getWithUser() {
+    void getWithUser() {
         Meal adminMeal = service.getWithUser(ADMIN_MEAL_ID, ADMIN_ID);
         MEAL_MATCHER.assertMatch(adminMeal, adminMeal1);
         USER_MATCHER.assertMatch(adminMeal.getUser(), admin);
     }
 
     @Test
-    public void getWithUserNotFound() {
-        Assert.assertThrows(NotFoundException.class,
+    void getWithUserNotFound() {
+        Assertions.assertThrows(NotFoundException.class,
                 () -> service.getWithUser(1, ADMIN_ID));
     }
 }
